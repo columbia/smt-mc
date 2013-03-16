e9b685faf1c2a9ea86d50450068dee248a8611d2
Heming Cui <heming@heming.(none)>
2013-03-16 15:45:43 -0400

    Added non-det-dbug-small.cfg

--- mc-tools/tests/ex2.c
+++ mc-tools/tests/ex2.c
@@ -1,7 +1,9 @@
 #include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "tern/user.h"
+#include <dbug/interposition/stubs.h>
 
 pthread_mutex_t mutex;
 pthread_mutex_t mutex2;
@@ -9,54 +11,73 @@ pthread_mutex_t mutex2;
 void *
 thread(void *args)
 {
+dbug_off();
   //assert(pthread_mutex_lock(&mutex) == 0);
   //printf("Critical section slave.\n");
   //assert(pthread_mutex_unlock(&mutex) == 0);
 
+dbug_on();
+  tern_non_det_start();
   int n = 0;
-  for (n = 0; n < 2; n++) {
-    tern_non_det_start();
+  //for (n = 0; n < 2; n++) {
     printf("\n\n\n\n\n\n\n\n\n============== start non-det self %u  =====================\n\n", (unsigned)pthread_self());
     int i = 0;
-    for (i = 0; i < 2; i++) {
+    //for (i = 0; i < 2; i++) {
       printf("\n\n============== start lock self %u  =====================\n\n", (unsigned)pthread_self());
       assert(pthread_mutex_lock(&mutex2) == 0);
       printf("Critical section slave2.\n");
       assert(pthread_mutex_unlock(&mutex2) == 0);
       printf("\n\n============== end lock self %u  =====================\n\n", (unsigned)pthread_self());
-    }
+    //}
     printf("\n\n============== end non-det self %u  =====================\n\n\n\n\n\n\n\n\n\n", (unsigned)pthread_self());
-    tern_non_det_end();
-  }
+  //}
+  tern_non_det_end();
+dbug_off();
 
+dbug_on();
+  //pthread_exit(0);
   return NULL;
 }
 
 int 
 main(int argc, char *argv[])
 {
-  const int nt = 2;
+  dbug_off();
+  const int nt = 3;
   //return 0;
   int i;
   pthread_t tid[nt];
   assert(pthread_mutex_init(&mutex,NULL) == 0);
 
+  dbug_on();
   tern_non_det_start();
   assert(pthread_mutex_init(&mutex2,NULL) == 0);
   tern_non_det_end();
-  //return 0;
+    dbug_off();
 
+    dbug_on();
   for (i = 0; i < nt; i++)
     assert(pthread_create(&tid[i],NULL,thread,NULL) == 0);
+  dbug_off();
 
-
-  //assert(pthread_mutex_lock(&mutex2) == 0);
+  dbug_on();
+  tern_non_det_start();
+  assert(pthread_mutex_lock(&mutex2) == 0);
   printf("Critical section master.\n");
-  //assert(pthread_mutex_unlock(&mutex2) == 0);
+  assert(pthread_mutex_unlock(&mutex2) == 0);
+  tern_non_det_end();
+dbug_off();
+
+    //sleep(10);
+  dbug_on();
   for (i = 0; i < nt; i++)
     assert(pthread_join(tid[i], NULL) == 0);
+  dbug_off();
+
   assert(pthread_mutex_destroy(&mutex) == 0);
 
+
+  dbug_on();
   return 0;
 }
 
