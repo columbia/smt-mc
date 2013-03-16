305091b4f0dfae764f81ed14f7cac45aedc9c6da
Heming Cui <heming@heming.(none)>
2013-03-15 21:38:23 -0400

    Nits to the test-dbug-on-off testcase

--- mc-tools/tests/test-dbug-on-off.c
+++ mc-tools/tests/test-dbug-on-off.c
@@ -6,29 +6,55 @@ pthread_mutex_t mutex;
 pthread_mutex_t mutex2;
 
 void *my_func(void *args) {
-  //dbug_off();
+  printf("child %ld start\n", (long)args);
+  dbug_off();
+  dbug_on();
   pthread_mutex_lock(&mutex);
   printf("child %ld\n", (long)args);
   pthread_mutex_unlock(&mutex);  
-  //dbug_on();
+
+  dbug_off();
+
   pthread_mutex_lock(&mutex2);
-  pthread_mutex_unlock(&mutex2);
+  printf("child2 %ld\n", (long)args);
+  pthread_mutex_unlock(&mutex2);  
+
+  dbug_on();
 }
 
 int main() {
+  dbug_off();
   const int nt = 2;
   pthread_t t[nt];
+
+  dbug_on();
   pthread_mutex_init(&mutex, NULL);
+  dbug_off();
+
   pthread_mutex_init(&mutex2, NULL);
-  for (int i = 0; i < nt; i++)
+
+  for (int i = 0; i < nt; i++) {
+    dbug_on();
     pthread_create(&t[i], NULL, my_func, (void *)i);
-  //dbug_off();
+    dbug_off();
+  }
+
+  dbug_on();
   pthread_mutex_lock(&mutex);
   printf("parent\n");
-  pthread_mutex_unlock(&mutex);
-  //dbug_on();
-  for (int i = 0; i < nt; i++)
+  pthread_mutex_unlock(&mutex);  
+  dbug_off();
+
+  for (int i = 0; i < nt; i++) {
+    dbug_on();
     pthread_join(t[i], NULL);
+    dbug_off();
+  }
+
+  dbug_on();
   pthread_mutex_destroy(&mutex);
+  dbug_off();
+
+  dbug_on();
   return 0;
 }
