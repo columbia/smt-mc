72586ef1707ee54347c41f041a662a9ed0af948e
Heming Cui <heming@heming.(none)>
2013-03-19 03:17:48 -0400

    Added 3 non-det benchmarks to non-det-dbug-small.cfg.

--- mc-tools/tests/openmp-like.cpp
+++ mc-tools/tests/openmp-like.cpp
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <semaphore.h>
+#include "tern/user.h"
 
 pthread_mutex_t mutex;
 sem_t sem;
@@ -10,13 +11,15 @@ void *my_func(void *args) {
   dbug_off();
   
 
-  //dbug_detach();
+  tern_non_det_start();
   dbug_on();
   pthread_mutex_lock(&mutex);  
    pthread_mutex_unlock(&mutex);
   dbug_off();
+  tern_non_det_end();
 
   dbug_detach();
+  tern_detach();
   fprintf(stderr, "child blocks.\n");
   sem_wait(&sem);
   fprintf(stderr, "child wakes up.\n");
@@ -33,8 +36,10 @@ int main() {
   dbug_on();
   pthread_create(&t, NULL, my_func, NULL);
 
+  tern_non_det_start();
 pthread_mutex_lock(&mutex);
    pthread_mutex_unlock(&mutex);
+  tern_non_det_end();
 
   dbug_off();
 
